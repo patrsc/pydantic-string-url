fdacc4e9a74f267c5f445bffc33ff748ec8f67e5
Patrick <patrsc@nubu.at>
2024-10-25 15:25:41 +0200

    Fix bug in script

--- publish.sh
+++ publish.sh
@@ -5,6 +5,6 @@
 # bash publish.sh
 
 set -eo pipefail
-poetry publish || echo "ERROR publish"
-git tag v$(poetry version -s) || echo "ERROR tag"
-git push --tags || echo "ERROR push tags"
+poetry publish
+git tag v$(poetry version -s)
+git push --tags
